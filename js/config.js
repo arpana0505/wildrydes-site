c232c1bd608faa31bcc7ff7bc5568a5cfa4e875a
arpana0505 <arpanak0505@gmail.com>
2026-07-02 18:19:09 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-2_2IzI7Lnk6', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '38feumemmmkpklkkk808u7787g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'wildrydes_secret_client_123', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
