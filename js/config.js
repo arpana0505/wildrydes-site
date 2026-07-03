cf3d834069f7c0aa3396ea391e08213eba5cc331
arpana0505 <arpanak0505@gmail.com>
2026-07-02 18:50:34 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_UsR7unscs', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'k80ubl74c3vbnlo5o2tcf4424', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_Jx0uOEcyK', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3s2tbs0catfinjin6tdeem5ig5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
